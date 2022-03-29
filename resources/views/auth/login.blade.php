f85d003f2454927dbb080fad8c42f9d38bc155b4
Sinan Sarıçayır <sinansaricayir@gmail.com>
2022-03-29 16:03:50 +0300

    Fix

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -24,6 +24,7 @@
                                 @enderror
                             </div>
                         </div>
+                        <br>
 
                         <div class="form-group row">
                             <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,6 +1,4 @@
-@extends('layouts.app')
 
-@section('content')
     <div class="breadcrumbs">
         <div class="container">
             <div class="breadcrumbs-main">
@@ -18,38 +16,6 @@
             <div class="register-top heading">
                 <h2>KAYIT OL</h2>
             </div>
-            <form method="POST" action="{{ route('register') }}">
-                @csrf
-            <div class="register-main">
-                <div class="col-md-6 account-left">
-                    <input class="@error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" placeholder="Adınız" type="text" tabindex="1" required>
-                    @error('name')
-                         <span class="invalid-feedback" role="alert">
-                             <strong>{{ $message }}</strong>
-                         </span>
-                    @enderror
-                    <input class="@error('name') is-invalid @enderror" name="email" value="{{ old('email') }}" placeholder="E-mail Adresiniz" type="text" tabindex="3" required>
-                    @error('email')
-                    <span class="invalid-feedback" role="alert">
-                             <strong>{{ $message }}</strong>
-                         </span>
-                    @enderror
-
-
-                    <input id="password" class="form-control @error('password') is-invalid @enderror" name="password" placeholder="Şifreniz" type="password" tabindex="4" required>
-                    @error('password')
-                       <span class="invalid-feedback" role="alert">
-                             <strong>{{ $message }}</strong>
-                       </span>
-                    @enderror
-                    <input id="password-confirm" type="password" placeholder="Şifreniz" name="password_confirmation" required autocomplete="new-password">
-                </div>
-                <div class="clearfix"></div>
-            </div>
-            <div class="address submit">
-                <input type="submit" value="Submit">
-            </div>
-            </form>
         </div>
     </div>
 
@@ -58,8 +24,3 @@
 
 
 
-
-
-
-
-@endsection
